7b65d0576cd7575dac45d23211c8c60065bde00f
Amid Mamedov <zuzukakabuba@gmail.com>
2025-02-01 21:52:41 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Amid Mamedov on 01.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
