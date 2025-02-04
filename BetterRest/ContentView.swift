883a2fa9b693ca63d76ed24d5a84647455707b6d
Amid Mamedov <zuzukakabuba@gmail.com>
2025-02-04 18:15:02 +0200

    datePicker

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUP = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        DatePicker("Please enter data", selection: $wakeUP, in: Date.now...)
+            .labelsHidden()
+    }
+    private func exampleDates() {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? .now
     }
 }
 
